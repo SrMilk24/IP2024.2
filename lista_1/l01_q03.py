b60245270833eec10f58dee8d08b858461aac97d
Felipe Leite <felipesd.leite@gmail.com>
2024-11-25 11:39:01 -0300

    feat: adding question 3 from conditionals list

---
+++ lista_1/l01_q03.py
@@ -0,0 +1,36 @@
+name_player_1 = input()
+player_1_points = int(input())
+
+name_player_2 = input()
+player_2_points = int(input())
+
+name_player_3 = input()
+player_3_points = int(input())
+
+winner = ''
+winner_points = 0
+
+if(player_1_points > player_2_points):
+    if(player_1_points > player_3_points):
+        winner = name_player_1
+        winner_points = player_1_points
+    else:
+        winner = name_player_3
+        winner_points = player_3_points
+else:
+    if(player_2_points > player_3_points):
+        winner = name_player_2
+        winner_points = player_2_points
+    else:
+        winner = name_player_3
+        winner_points = player_3_points
+
+
+print('O melhor jogador do mundo é {}, com {} ponto(s).'.format(winner, winner_points))
+
+if(winner == 'Vini Jr.'):
+    print('Os brasileiros amaram o resultado! BAILA VINI!')
+elif(winner == 'Rodri'):
+    print('Os europeus, como sempre, roubaram o nosso ouro!')
+else:
+    print('Essa premiação perdeu o sentido mesmo, como que o Vini Jr. não ganhou? Muito roubado!')
