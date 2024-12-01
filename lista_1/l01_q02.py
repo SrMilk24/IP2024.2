8e2e0ad2ee3256d6a2dccbb68863c922781e748c
SrMilk24 <fsl8@cin.ufpe.br>
2024-12-01 19:15:13 -0300

    fix: resolving problem from question 2

--- lista_1/l01_q02.py
+++ lista_1/l01_q02.py
@@ -2,16 +2,18 @@ chooser_1 = input()
 chooser_2 = input()
 winner = input()
 
-if (winner == "Palé"):
-        print("Acabou!!! o Brasil faz a festa e a paz no mundo é selada! PALÉ É O MELHOR DO MUNDO!")
+if (winner == "Palé" or ((chooser_1 == 'Gavião Bonito' or chooser_2 == 'Gavião Bonito') and (chooser_1 == 'Ronaldinho Paulisa' or chooser_2 == 'Ronaldinho Paulista'))):
+    print("Acabou!!! o Brasil faz a festa e a paz no mundo é selada! PALÉ É O MELHOR DO MUNDO!")
 elif (winner == "Mãodona"):
-    if (chooser_1 == "Gavião Bonito" and chooser_2 != "Ronaldinho Paulista"):
-        print("Gavião soltou seu grito, mas não foi o suficiente para dar o troféu ao rei do fut. Triste dia para o futebol mundial…")
-    elif (chooser_1 != "Gavião Bonito" and chooser_2 == "Ronaldinho Paulista"):
-        print("Paulista subiu no palco sozinho e ninguém sabe o porquê. Mais um momento aleatório e mais um dia triste para o brasileiro…")
-    elif (chooser_1 == "Gavião Bonito" and chooser_2 == "Ronaldinho Paulista"):
-        print("Acabou!!! o Brasil faz a festa e a paz no mundo é selada! PALÉ É O MELHOR DO MUNDO!")
+    if(chooser_1 == 'Gavião Bonito' or chooser_2 == 'Gavião Bonito'):
+        if(chooser_1 == 'Ronaldinho Paulista' or chooser_2 == 'Ronaldinho Paulista'):
+            print("Acabou!!! o Brasil faz a festa e a paz no mundo é selada! PALÉ É O MELHOR DO MUNDO!")
+        else:
+            print('Gavião soltou seu grito, mas não foi o suficiente para dar o troféu ao rei do fut. Triste dia para o futebol mundial…')
     else:
-        print("PERDEMOS! O futebol foi roubado e não há mais chance de volta por enquanto.")
+        if(chooser_1 == 'Ronaldinho Paulista' or chooser_2 == 'Ronaldinho Paulista'):
+            print('Paulista subiu no palco sozinho e ninguém sabe o porquê. Mais um momento aleatório e mais um dia triste para o brasileiro…')
+        else:
+            print('PERDEMOS! O futebol foi roubado e não há mais chance de volta por enquanto.')
 else:
     print("Não foi dessa vez, mas pelo menos não perdemos a nossa dignidade.")
