2d8ed6318af25d0442fb8f6d016860dbaa9fce0e
Felipe Milk <fsl8@cin.ufpe.br>
2024-12-02 13:39:10 -0300

    fix: trying resolve problems from question 9

--- lista_1/l01_q09.py
+++ lista_1/l01_q09.py
@@ -1,45 +1,43 @@
-suspect_1 = input()
-dist_susp_1 = int(input())
-suspect_2 = input()
-dist_susp_2 = int(input())
+nome_sus1 = input()
+distancia_sus1 = int(input())
+nome_sus2 = input()
+distancia_sus2 = int(input())
 
-distance = (dist_susp_1 ** 2 + dist_susp_2 ** 2) ** 0.5
+distancia = (((distancia_sus1 ** 2) + (distancia_sus2 ** 2)) ** 0.5)
 
-if(distance % 2 == 0):
-    print('{} e {} são CÚMPLICES no crime! Se juntaram para confundir a polícia, mas os CIners são mais espertos! A Bola de Ouro será recuperada com sucesso!'.format(suspect_1, suspect_2))
+
+if(distancia % 2 == 0):
+    print('{} e {} são CÚMPLICES no crime! Se juntaram para confundir a polícia, mas os CIners são mais espertos! A Bola de Ouro será recuperada com sucesso!'.format(nome_sus1, nome_sus2))
 else:
-    category_1 = input()
-    category_2 = input()
-
-    if(category_1 == 'Fã' and category_2 == 'Fã'):
-        if(dist_susp_1 % 2 == 0) and (dist_susp_1 % 3 == 0) and (dist_susp_2 % 2 == 0) and (dist_susp_2 % 3 == 0):
-            print('{} e {} são CÚMPLICES no crime! Se juntaram para confundir a polícia, mas os CIners são mais espertos! A Bola de Ouro será recuperada com sucesso!'.format(suspect_1, suspect_2))
-        elif(dist_susp_1 % 2 == 0):
-            print('{} é o(a) CULPADO(A)! Ele(a) confessou a localização da Bola de Ouro e ela foi retornada com sucesso!'.format(suspect_2))
-        elif(dist_susp_2 % 3 == 0):
-            print('{} é o(a) CULPADO(A)! Ele(a) confessou a localização da Bola de Ouro e ela foi retornada com sucesso!'.format(suspect_1))
-            
-    elif(category_1 == 'Jogador') and (category_2 == 'Jogador'):
-        if(dist_susp_1 % 2 == 0) and (dist_susp_1 % 5 == 0) and (dist_susp_2 % 2 == 0) and (dist_susp_2 % 5 == 0):
-            print('{} e {} são CÚMPLICES no crime! Se juntaram para confundir a polícia, mas os CIners são mais espertos! A Bola de Ouro será recuperada com sucesso!'.format(suspect_1, suspect_2))
-        elif(dist_susp_1 % 2 == 0):
-            print('{} é o(a) CULPADO(A)! Ele(a) confessou a localização da Bola de Ouro e ela foi retornada com sucesso!'.format(suspect_2))
-        elif(dist_susp_2 % 5 == 0):
-            print('{} é o(a) CULPADO(A)! Ele(a) confessou a localização da Bola de Ouro e ela foi retornada com sucesso!'.format(suspect_1))
-        
-
-    elif(category_1 == 'Jornalista') and (category_2 == 'Jornalista'):
-        if(dist_susp_1 % 3 == 0) and (dist_susp_1 % 5 == 0) and (dist_susp_2 % 3 == 0) and (dist_susp_2 % 5 == 0):
-            print('{} e {} são CÚMPLICES no crime! Se juntaram para confundir a polícia, mas os CIners são mais espertos! A Bola de Ouro será recuperada com sucesso!'.format(suspect_1, suspect_2))
-        elif(dist_susp_1 % 3 == 0):
-            print('{} é o(a) CULPADO(A)! Ele(a) confessou a localização da Bola de Ouro e ela foi retornada com sucesso!'.format(suspect_2))
-        elif(dist_susp_2 % 5 == 0):
-            print('{} é o(a) CULPADO(A)! Ele(a) confessou a localização da Bola de Ouro e ela foi retornada com sucesso!'.format(suspect_1))
-            
+    categoria1 = input()
+    categoria2 = input()
+    
+    if(categoria1 == categoria2):
+        if(categoria1 == 'Fã'):
+            if(distancia_sus1 % 2 == 0 and distancia_sus1 % 3 == 0 and distancia_sus2 % 2 == 0 and distancia_sus2 % 3 == 0):
+                print('{} e {} são CÚMPLICES no crime! Se juntaram para confundir a polícia, mas os CIners são mais espertos! A Bola de Ouro será recuperada com sucesso!'.format(nome_sus1, nome_sus2))
+            elif(distancia_sus1 % 2 == 0):
+                print('{} é o(a) CULPADO(A)! Ele(a) confessou a localização da Bola de Ouro e ela foi retornada com sucesso!'.format(nome_sus2))
+            elif(distancia_sus2 % 3 == 0):
+                print('{} é o(a) CULPADO(A)! Ele(a) confessou a localização da Bola de Ouro e ela foi retornada com sucesso!'.format(nome_sus1))
+        elif(categoria1 == 'Jogador'):
+            if(distancia_sus1 % 2 == 0 and distancia_sus1 % 5 == 0 and distancia_sus2 % 2 == 0 and distancia_sus2 % 5 == 0):
+                print('{} e {} são CÚMPLICES no crime! Se juntaram para confundir a polícia, mas os CIners são mais espertos! A Bola de Ouro será recuperada com sucesso!'.format(nome_sus1, nome_sus2))
+            elif(distancia_sus1 % 2 == 0):
+                print('{} é o(a) CULPADO(A)! Ele(a) confessou a localização da Bola de Ouro e ela foi retornada com sucesso!'.format(nome_sus2))
+            elif(distancia_sus2 % 5 == 0):
+                print('{} é o(a) CULPADO(A)! Ele(a) confessou a localização da Bola de Ouro e ela foi retornada com sucesso!'.format(nome_sus1))
+        elif(categoria1 == 'Jornalista'):
+            if(distancia_sus1 % 3 == 0 and distancia_sus1 % 5 == 0 and distancia_sus2 % 3 == 0 and distancia_sus2 % 5 == 0):
+                print('{} e {} são CÚMPLICES no crime! Se juntaram para confundir a polícia, mas os CIners são mais espertos! A Bola de Ouro será recuperada com sucesso!'.format(nome_sus1, nome_sus2))
+            elif(distancia_sus1 % 3 == 0):
+                print('{} é o(a) CULPADO(A)! Ele(a) confessou a localização da Bola de Ouro e ela foi retornada com sucesso!'.format(nome_sus2))
+            elif(distancia_sus2 % 5 == 0):
+                print('{} é o(a) CULPADO(A)! Ele(a) confessou a localização da Bola de Ouro e ela foi retornada com sucesso!'.format(nome_sus1))
     else:
-        if(dist_susp_1 == dist_susp_2):
-            print('{} e {} são INOCENTES! A polícia parisiense não fez um bom trabalho... Vamos continuar procurando!'.format(suspect_1, suspect_2))
-        elif(dist_susp_1 > dist_susp_2):
-            print('{} é o(a) CULPADO(A)! Ele(a) confessou a localização da Bola de Ouro e ela foi retornada com sucesso!'.format(suspect_2))
+        if(distancia_sus1 == distancia_sus2):
+            print('{} e {} são INOCENTES! A polícia parisiense não fez um bom trabalho... Vamos continuar procurando!'.format(nome_sus1, nome_sus2))
+        elif(distancia_sus1 > distancia_sus2):
+            print('{} é o(a) CULPADO(A)! Ele(a) confessou a localização da Bola de Ouro e ela foi retornada com sucesso!'.format(nome_sus2))
         else:
-            print('{} é o(a) CULPADO(A)! Ele(a) confessou a localização da Bola de Ouro e ela foi retornada com sucesso!'.format(suspect_1))
+            print('{} é o(a) CULPADO(A)! Ele(a) confessou a localização da Bola de Ouro e ela foi retornada com sucesso!'.format(nome_sus1))
