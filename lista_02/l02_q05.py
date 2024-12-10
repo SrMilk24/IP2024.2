0e75c57e8d4bf3fc7002cbb4d1e650ff1cadf790
Felipe Milk <fsl8@cin.ufpe.br>
2024-12-10 13:55:59 -0300

    fix: adding more things do question 05, theres a error on attributions on case 3

--- lista_02/l02_q05.py
+++ lista_02/l02_q05.py
@@ -47,9 +47,27 @@ for x in range(0,3):
                     menor_stream = quantidade_streams
                     terceira_musica = nome_musica
 
-print(primeira_musica, segunda_musica, terceira_musica)
+print('1º: {} teve {} de streams e foi a música mais ouvida de Simone!'.format(primeira_musica, maior_stream))
+print('2º: {} teve {} de streams e foi a música mais ouvida de Simone!'.format(segunda_musica, media_stream))
+print('3º: {} teve {} de streams e foi a música mais ouvida de Simone!'.format(terceira_musica, menor_stream))
                 
-if(maior_stream > 10000000 or menor_stream > 10000000  or media_stream > 10000000):
-    if(maior_stream > 10000000):
-        print('Uau! {} foi um hit certeiro da rainha do Natal!'.format(primeira_musica))
+
+if(maior_stream > 10000000):
+    print('Uau! {} foi um hit certeiro da rainha do Natal!'.format(primeira_musica))
+elif(maior_stream < 1000000):
+    print('Bom… a música {} não foi exatamente um sucesso nacional, mas foi o suficiente para entrar no topo da lista das mais ouvidas.'.format(primeira_musica))
+
+if(menor_stream > 10000000):
+    print('Uau! {} foi um hit certeiro da rainha do Natal!'.format(terceira_musica))
+elif(menor_stream < 1000000):
+    print('Bom… a música {} não foi exatamente um sucesso nacional, mas foi o suficiente para entrar no topo da lista das mais ouvidas.'.format(terceira_musica))
     
+if(media_stream > 10000000):
+    print('Uau! {} foi um hit certeiro da rainha do Natal!'.format(segunda_musica))
+elif(media_stream < 1000000):
+    print('Bom… a música {} não foi exatamente um sucesso nacional, mas foi o suficiente para entrar no topo da lista das mais ouvidas.'.format(segunda_musica))    
+
+if(maior_stream - media_stream > 5000000):
+    print('{} foi disparada a mais ouvida neste ano! Nenhuma outra música natalina chega aos pés dela!'.format(primeira_musica))
+if(maior_stream - media_stream < 1000000):
+    print('{} foi a mais ouvida, mas não podemos esquecer da música {}! Fez bonito nesse feriado!'.format(primeira_musica, segunda_musica))
