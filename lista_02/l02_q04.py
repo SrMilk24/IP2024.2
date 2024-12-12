09155d2191fc81c016e7de87c07e0f825458b12f
Felipe Milk <fsl8@cin.ufpe.br>
2024-12-12 09:34:19 -0300

    fix: finishing question 04 from list 02

--- lista_02/l02_q04.py
+++ lista_02/l02_q04.py
@@ -2,6 +2,9 @@ quant_folhas = int(input())
 populacao = int(input())
 altura = int(input())
 
+folhas_necessarias = 0
+
+
 folhas_pessoa = quant_folhas / populacao
 
 if(folhas_pessoa < 6):
@@ -9,8 +12,20 @@ if(folhas_pessoa < 6):
 else:
     #formula -> h = (-1 + raiz(1+8x))/2
     folhas_arvore = folhas_pessoa / 2
-    print('{}*'.format(' '*(altura)))
-    for x in range(0, altura+1):
-        print('{}{}⠀{}'.format(' '*(altura-x),'+'*x, '+'*x))
+    for x in range(1, altura+1):
+        folhas_necessarias += x
+    if(folhas_necessarias > quant_folhas):
+        print('Infelizmente não poderemos comemorar o Natal esse ano, não conseguimos fazer uma única árvore!')
+    else:
+        print('{}*'.format(' '*(altura)))
+        for x in range(1, altura+1):
+            print('{}{}⠀{}'.format(' '*(altura-x),'+'*x, '+'*x))
+            
+    arvores_populacao = populacao * folhas_necessarias
 
+    if(quant_folhas >= arvores_populacao):
+        print('O Grinch não conseguiu estragar o Natal dessa vez!')
+    else:
+        print('Essa árvore está muito grande, dessa forma não conseguiremos entregar para a cidade toda')
+    
 
