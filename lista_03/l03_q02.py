7d252d2e91199573ccf96f653671fd41c8ca3061
fsl8 <fsl8@g1c14.cin.ufpe.br>
2025-01-31 19:46:39 -0300

    feat: adding question 02

--- lista_03/l03_q02.py
+++ lista_03/l03_q02.py
@@ -1,5 +1,33 @@
 entrada = input()
+nomes = []
+for x in entrada.split("-"):
+    nomes.append(x)
+
 validador = ""
+nomes_trocados = []
 
 while validador != "Acabou!":
     troca = input()
+    if troca == "Acabou!":
+        validador = "Acabou!"
+    else:
+        troca_separada = troca.split("-")
+        if troca_separada[0] not in nomes or troca_separada[1] not in nomes:
+            print("Essa dupla não esta na lista!")
+        else:
+            nome_1 = nomes.index(troca_separada[0])
+            nome_2 = nomes.index(troca_separada[1])
+
+            nomes[nome_1] = troca_separada[1]
+            nomes[nome_2] = troca_separada[0]
+            print(nomes)
+            for x in troca_separada:
+                nomes_trocados.append(x)
+            
+print("Fila do almoço:")
+for nome in nomes:
+    count = nomes_trocados.count(nome)
+    if count > 1:
+        print(f"{nome}: {count} teleportes!")
+    elif count <= 1:
+        print(f"{nome}: {count} teleporte!")
