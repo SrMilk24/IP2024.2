7cba4ecbdd5e9d5e6931b977fb9a5e4371632898
Felipe Milk <fsl8@cin.ufpe.br>
2025-02-04 14:10:32 -0300

    fix: trying not kill myself on this question 10

--- lista_03/l03_q10.py
+++ lista_03/l03_q10.py
@@ -18,14 +18,15 @@ for golpe in golpes:
     if golpe != "expansão de domínio" and golpe != "black flash":
         contador_golpes.append([golpe, 0])
 
-if reversao_feitico == "False":
-    print("Exorcizar o Mahoraga sem conseguir me curar vai ser bem difícil, mas eu não tenho escolha!")
-else:
+if reversao_feitico == "True":
     print("Mesmo com a regeneração, ainda não vai ser fácil! Vamos nessa!")
+else:
+    print("Exorcizar o Mahoraga sem conseguir me curar vai ser bem difícil, mas eu não tenho escolha!")
 
 while vida_feiticeiro > 0 and vida_mahoraga > 0:
     mov_feiticeiro = input()
     mov_mahoraga = input()
+    
 
     ataque_anterior = mov_feiticeiro
 
@@ -48,8 +49,9 @@ while vida_feiticeiro > 0 and vida_mahoraga > 0:
 
     if mov_feiticeiro == "reversão de feitiço" and reversao_feitico == "True":
         print("Eu posso continuar lutando mais um pouco...")
+        vida_feiticeiro += 25
     
-    if mov_feiticeiro in golpes and mov_feiticeiro != "black flash" and mov_feiticeiro != "expansão de domínio":
+    if mov_feiticeiro in golpes and mov_feiticeiro != "black flash" and mov_feiticeiro != "expansão de domínio" and mov_feiticeiro != "reversão de feitiço":
         index_golpe = golpes.index(mov_feiticeiro)
 
         full_adaptado = 0
@@ -65,37 +67,57 @@ while vida_feiticeiro > 0 and vida_mahoraga > 0:
             if contador_golpes[index_golpe][1] == 0:
                 vida_mahoraga -= (ataque_feiticeiro - defesa_mahoraga) + 25
                 print(f"A roda do Mahoraga girou uma vez! {mov_feiticeiro} só vai funcionar mais duas vezes")
+                
                 contador_golpes[index_golpe][1] += 1
             elif contador_golpes[index_golpe][1] == 1:
-                vida_mahoraga -= ((ataque_feiticeiro - defesa_mahoraga) + 25) / 2
+                vida_mahoraga -= ((ataque_feiticeiro - defesa_mahoraga) + 25) // 2
                 print(f"A roda do Mahoraga girou pela segunda vez! {mov_feiticeiro} só vai funcionar mais uma vez")
+                
                 contador_golpes[index_golpe][1] += 1
             elif contador_golpes[index_golpe][1] == 2:
                 print(f"A roda do Mahoraga girou pela terceira vez! {mov_feiticeiro} não vai funcionar mais")
-                vida_mahoraga -= ((ataque_feiticeiro - defesa_mahoraga) + 25) / 4
+                
+                vida_mahoraga -= ((ataque_feiticeiro - defesa_mahoraga) + 25) // 4
                 contador_golpes[index_golpe][1] += 1
             elif contador_golpes[index_golpe][1] > 3:
                 print("Esse ataque é inútil! Melhor tentar outra coisa.")
 
     
-    if vida_mahoraga >= 0:
-        if mov_mahoraga in mahoraga_golpes:
-            if mov_mahoraga == "regeneração":
-                vida_mahoraga += 25
-                print("Ele está se regenerando.")
-            elif mov_mahoraga == "adaptação":
-                if ataque_anterior != "reversão de feitiço":
-                    if ataque_anterior != "black flash":
-                        index_golpe = golpes.index(ataque_anterior)
-                        contador_golpes[index_golpe][1] += 1  # Incrementar adaptação corretamente
-                        if contador_golpes[index_golpe][1] == 1:
-                            print(f"A roda do Mahoraga girou pela segunda vez! {ataque_anterior} só vai funcionar mais uma vez")
-                        elif contador_golpes[index_golpe][1] == 2:
-                            print(f"A roda do Mahoraga girou pela terceira vez! {ataque_anterior} não vai funcionar mais")
-                    else:
-                        print("Nem você vai conseguir se adaptar a isso, mahoraga!")
-            elif mov_mahoraga == "ataque":
-                vida_feiticeiro -= (ataque_mahoraga - defesa_feiticeiro) + 25
+        if vida_mahoraga > 0:
+
+            if mov_mahoraga in mahoraga_golpes:
+
+                if mov_mahoraga == "regeneração":
+
+                    vida_mahoraga += 25
+
+                    print("Ele está se regenerando.")
+
+                elif mov_mahoraga == "adaptação":
+
+                    if ataque_anterior != "reversão de feitiço":
+
+                        if ataque_anterior != "black flash":
+
+                            index_golpe = golpes.index(ataque_anterior)
+
+                            contador_golpes[index_golpe][1] += 1  # Incrementar adaptação corretamente
+
+                            if contador_golpes[index_golpe][1] == 1:
+
+                                print(f"A roda do Mahoraga girou pela segunda vez! {ataque_anterior} só vai funcionar mais uma vez")
+
+                            elif contador_golpes[index_golpe][1] == 2:
+
+                                print(f"A roda do Mahoraga girou pela terceira vez! {ataque_anterior} não vai funcionar mais")
+
+                        else:
+
+                            print("Nem você vai conseguir se adaptar a isso, mahoraga!")
+
+                elif mov_mahoraga == "ataque":
+
+                    vida_feiticeiro -= (ataque_mahoraga - defesa_feiticeiro) + 25
 
 if vida_mahoraga <= 0:
     print(f"{nome_feiticeiro} conseguiu!")
