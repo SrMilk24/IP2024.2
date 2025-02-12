72d826316d1bd2aa070e7fb31f947a819ec36669
fsl8 <fsl8@g5c48.cin.ufpe.br>
2025-02-12 18:40:55 -0300

    feat: adding question 04, almost done

--- lista_04/l04_q04.py
+++ lista_04/l04_q04.py
@@ -1,12 +1,43 @@
 alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
+#nome_alvo = input()
+#precisao = int(input())
+#resistencia = int(input())
+#poder = int(input())
+menssagem_cifrada = input()
+deslocamento = int(input())
 
-def descifra_ataque(nome_alvo, precisao, resistencia, poder, formula, deslocamento):
-    dano = precisao * (poder / resistencia) * formula
 
 
+def descriptografar(palavra_cifrada, deslocamento):
+    cifra_to_list = list(palavra_cifrada)
+    for letra in range(len(cifra_to_list)):
+        index_atual = alfabeto.index(cifra_to_list[letra])
+        novo_index = (index_atual - deslocamento) % 26
+        cifra_to_list[letra] = alfabeto[novo_index]
 
-num_ataques = int(input())
+    print(f"Decifrando: {''.join(cifra_to_list)}")
 
-if num_ataques == 0:
-    print("Piltover em paz... por enquanto.")
+    return ''.join(cifra_to_list)
+
+def calcular_dano(precisao, poder, resistencia, fator):
+    dano = precisao * (poder / resistencia)
+
+    if fator == "ALTO":
+        dano *= 2
+    elif fator == "MEDIO":
+        dano *= 1.5
+    elif fator == "BAIXO":
+            dano *= 1
+    return round(dano)
+
+def resultado(danos):
+    danos.sort(key=lambda item: item[0])
+    for x in len(danos):
+        print(f"{danos[x][1]}: {danos[x][0]} de dano calculado.")
+        if danos[x][0] >= 100:
+            print(f"{danos[x][1]} será destruído!")
+        else:
+            print(f"{danos[x][1]} resistirá ao ataque.")
+        
+descriptografia = descriptografar(menssagem_cifrada, deslocamento)
