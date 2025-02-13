32b1d89d79af1eb6b3d774cf6d41197af3744528
Felipe Milk <fsl8@cin.ufpe.br>
2025-02-12 23:12:25 -0300

    fix: solving problems from question 04

--- lista_04/l04_q04.py
+++ lista_04/l04_q04.py
@@ -1,22 +1,11 @@
 alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-#nome_alvo = input()
-#precisao = int(input())
-#resistencia = int(input())
-#poder = int(input())
-menssagem_cifrada = input()
-deslocamento = int(input())
-
-
-
 def descriptografar(palavra_cifrada, deslocamento):
     cifra_to_list = list(palavra_cifrada)
     for letra in range(len(cifra_to_list)):
         index_atual = alfabeto.index(cifra_to_list[letra])
         novo_index = (index_atual - deslocamento) % 26
         cifra_to_list[letra] = alfabeto[novo_index]
-
-    print(f"Decifrando: {''.join(cifra_to_list)}")
 
     return ''.join(cifra_to_list)
 
@@ -28,16 +17,43 @@ def calcular_dano(precisao, poder, resistencia, fator):
     elif fator == "MEDIO":
         dano *= 1.5
     elif fator == "BAIXO":
-            dano *= 1
+        dano *= 1
     return round(dano)
 
 def resultado(danos):
-    danos.sort(key=lambda item: item[0])
-    for x in len(danos):
-        print(f"{danos[x][1]}: {danos[x][0]} de dano calculado.")
-        if danos[x][0] >= 100:
-            print(f"{danos[x][1]} será destruído!")
+
+    for dano, nome_alvo, formula in danos:
+        print(f"Decifrando: {formula}")
+        print(f"{nome_alvo}: {dano} de dano calculado.")
+        if dano >= 100:
+            print(f"{nome_alvo} será destruído!")
         else:
-            print(f"{danos[x][1]} resistirá ao ataque.")
+            print(f"{nome_alvo} resistirá ao ataque.")
+        print()
         
-descriptografia = descriptografar(menssagem_cifrada, deslocamento)
+    danos.sort(key=lambda item: item[0], reverse=True)
+
+    print("Prioridade de ataques:")
+    for dano, nome_alvo, _ in danos:
+        print(f"{nome_alvo} - {dano} de dano")
+
+ataques = int(input())
+
+
+
+if ataques == 0:
+    print("Piltover em paz... por enquanto.")
+else:
+    danos = []
+    for x in range(ataques):
+        nome_alvo, precisao, resistencia, poder, menssagem_cifrada, deslocamento = input().split(", ")
+    
+        precisao = int(precisao)
+        resistencia = int(resistencia)
+        poder = int(poder)
+        deslocamento = int(deslocamento)
+
+        descriptografia = descriptografar(menssagem_cifrada, deslocamento)
+        dano = calcular_dano(precisao, poder, resistencia, descriptografia)
+        danos.append([dano, nome_alvo, descriptografia])
+    resultado(danos)
