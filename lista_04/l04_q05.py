28f59a54005b50aad2aee4a43eaf19e44ad233c2
Felipe Milk <fsl8@cin.ufpe.br>
2025-02-13 13:22:44 -0300

    feat: adding new functions from question 05

--- lista_04/l04_q05.py
+++ lista_04/l04_q05.py
@@ -10,13 +10,42 @@ inimigo = [nome, vida, distancia, velocidade, defesa]
 def use_zap(inimigo):
     
     if inimigo[4] == 1:
-        if inimigo[2] > 1:
-            inimigo[3] -= 1
-            inimigo[2] -= inimigo[3]
+        inimigo[3] -= 1
     elif inimigo[4] == 0:
-        if inimigo[2] > 1:
-            inimigo[1] -= 5
-            inimigo[3] -= 1
-            inimigo[2] -= inimigo[3]
-            
+        inimigo[1] -= 5
+        inimigo[3] -= 1 
+               
     return inimigo
+
+def use_powpow(inimigo):
+    if inimigo[4] == 0:
+        inimigo[1] -= 15
+        
+    return inimigo
+
+def use_fishbones(inimigo):
+    if inimigo[4] == 1:
+        inimigo[4] = 0 
+    elif inimigo[4] == 0:
+        inimigo[1] -= 30
+        
+    return inimigo
+
+def use_ultimate(inimigo):
+    if inimigo[4] == 0:
+        inimigo[1] -= 100
+        
+    return inimigo
+
+def calcula_distancia(inimigo):
+    inimigo[2] -= inimigo[3]
+    
+    return inimigo
+
+def verifica_distancia(inimigo):
+    if inimigo[2] >= 1:
+        return True
+    else:
+        return False
+
+    
