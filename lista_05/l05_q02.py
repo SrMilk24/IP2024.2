81973d0e02b295224af286e0c9685e187bdf8e91
fsl8 <fsl8@g5c43.cin.ufpe.br>
2025-02-26 18:06:03 -0300

    feat: adding question 02

---
+++ lista_05/l05_q02.py
@@ -0,0 +1,11 @@
+entrada = input().split(',')
+
+def caca_tesouro(sequencia, posicao = 0):
+    valores = sequencia.strip(',')
+    if len(sequencia) > 0:
+        #if sequencia[posicao]
+        return
+
+    return True
+
+caca_tesouro("123445")
