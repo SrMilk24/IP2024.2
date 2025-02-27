0caff1a2d66d5e094c0de2e4b46ac51a512cc0a7
Felipe Leite <fsl8@cin.ufpe.br>
2025-02-27 12:43:47 -0300

    feat: adding more structures on code of question 2

--- lista_05/l05_q02.py
+++ lista_05/l05_q02.py
@@ -1,11 +1,22 @@
-entrada = input().split(',')
+#entrada = input().split(',')
 
-def caca_tesouro(sequencia, posicao = 0):
-    valores = sequencia.strip(',')
-    if len(sequencia) > 0:
-        #if sequencia[posicao]
-        return
+def caca_tesouro(sequencia, posicao = 0, energia = 4, tesouro = 0, moeda = 0):
+    valores = sequencia.split(', ')
+    if len(valores) > 0:
+        if energia > 0:
+            if len(valores) > posicao:
+                print(valores[1])
 
-    return True
+                if valores[posicao] == "X":
+                    caca_tesouro(sequencia, posicao+1, energia-1, tesouro, moeda)
+                elif valores[posicao].isnumeric():
+                    caca_tesouro(sequencia, posicao+1, energia-1, tesouro, moeda+1)
+                else:
+                    caca_tesouro(sequencia, posicao+1, energia+1, tesouro+1, moeda)
+        else:
+            print("Ah, cansei. Vou descansar.")
+            return [tesouro, moeda]
 
-caca_tesouro("123445")
+    return [tesouro, moeda]
+
+print(caca_tesouro("X, 5, 5 X, 2, 9, X "))
