76b0314e8a0f2b0b2114eb4647450b49966034f2
Felipe Milk <fsl8@cin.ufpe.br>
2025-03-04 00:17:40 -0300

    feat: solving question 2 from list 05

--- lista_05/l05_q02.py
+++ lista_05/l05_q02.py
@@ -1,22 +1,31 @@
-#entrada = input().split(',')
-
 def caca_tesouro(sequencia, posicao = 0, energia = 4, tesouro = 0, moeda = 0):
-    valores = sequencia.split(', ')
-    if len(valores) > 0:
+    if len(sequencia) > 0:
         if energia > 0:
-            if len(valores) > posicao:
-                print(valores[1])
-
-                if valores[posicao] == "X":
-                    caca_tesouro(sequencia, posicao+1, energia-1, tesouro, moeda)
-                elif valores[posicao].isnumeric():
-                    caca_tesouro(sequencia, posicao+1, energia-1, tesouro, moeda+1)
+            if len(sequencia) > posicao:
+                if sequencia[posicao] == "X":
+                    return caca_tesouro(sequencia, posicao+1, energia-1, tesouro, moeda)
+                elif sequencia[posicao].isnumeric():
+                    return caca_tesouro(sequencia, posicao+1, energia-1, tesouro, moeda+int(sequencia[posicao]))
                 else:
-                    caca_tesouro(sequencia, posicao+1, energia+1, tesouro+1, moeda)
+                    print(f"Oba! Encontrei um {sequencia[posicao]} 🐧🎉")
+                    return caca_tesouro(sequencia, posicao+1, energia+1, tesouro+1, moeda)
+                    
         else:
             print("Ah, cansei. Vou descansar.")
             return [tesouro, moeda]
 
     return [tesouro, moeda]
 
-print(caca_tesouro("X, 5, 5 X, 2, 9, X "))
+entrada = input().split(',')
+entrada = [item.strip() for item in entrada]
+
+resultado = caca_tesouro(entrada)
+
+if resultado[0] == 0 and resultado[1] == 0:
+    print("Essa caminhada não foi nada produtiva. É melhor ir pescar.")
+elif resultado[0] != 0 and resultado[1] == 0:
+    print(f"Dinheiro? Não temos. Mas temos {resultado[0]} itens raros.")
+elif resultado[0] == 0 and resultado[1] != 0:
+    print(f"Estamos ricos!! Encontrei {resultado[1]} moedas.")
+else:
+    print(f"Quem diria!! {resultado[1]} moedas e {resultado[0]} itens raros. Hoje eu mereço bolo, não aqueles puffitos de sempre.")
