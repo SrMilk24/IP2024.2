eaaa180a25dafb37acc49329b665ce6e57648f9c
fsl8 <fsl8@g5c37.cin.ufpe.br>
2025-03-17 19:56:48 -0300

    feat: adding new features from question

--- lista_06/l06_q06.py
+++ lista_06/l06_q06.py
@@ -34,14 +34,21 @@ armas = {
 def verifica_estado(lista_estados, estado):
     return estado in lista_estados
 
-def calcular_forca(dic_armas, ouro, soldados, armas):
+def verificar_arma(armas, arma):
+    return arma in armas
+
+def calcular_forca(dic_armas, ouro, soldados, armas, estado):
     letalidade_armas = 0
     
     for arma in armas:
         letalidade_armas += dic_armas.get(arma)
     forca = ((ouro * 2) + (soldados * 4) + (letalidade_armas * 4)) / 10
 
+    if estado == 'PE' or estado == 'PB' or estado == 'RN':
+        forca *= 1.1
+        print(f"o estado de {estado} ganhou 10% de força pois está lutando em um campo de batalha que lhe confere vantagem!")
     return forca
 
 def calcular_dano_batalha(estado_atacante, ouro_atacante, soldados_atacante, armas_atacante, estado_defensor, ouro_defensor, soldados_defensor, armas_defensor):
-    ...
+    if verifica_estado(estado_atacante not in estados_alianca) and verifica_estado(estado_atacante not in estados_coroa):
+        print("@ estado não encontrado ou não faz parte das regiões em guerra! @")
