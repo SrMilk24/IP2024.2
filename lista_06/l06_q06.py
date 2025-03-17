eb419d74d0229281a5631f9adceb03da9a9776df
fsl8 <fsl8@g5c37.cin.ufpe.br>
2025-03-17 19:44:56 -0300

    feat: adding functions verificar_estado, calcular_forca from question 06

---
+++ lista_06/l06_q06.py
@@ -0,0 +1,47 @@
+estados_alianca = ('PE', 'PB', 'RN', 'CE', 'AL', 'BA')
+anexados_alianca = []
+estados_coroa = ('SP', 'RJ', 'ES', 'MG', 'PR', 'RS')
+anexados_coroa = []
+
+alianca = {
+    'estados': estados_alianca,
+    'pontos_vida': 1000,
+    'riqueza': 20000,
+    'quantidade_soldados': 10000,
+    'quantidade_vitorias': 0,
+    'quantidade_derrotas': 1,
+    'estados_anexados': anexados_alianca
+}
+
+coroa = {
+    'estados': estados_coroa,
+    'pontos_vida': 1000,
+    'riqueza': 20000,
+    'quantidade_soldados': 10000,
+    'quantidade_vitorias': 0,
+    'quantidade_derrotas': 1,
+    'estados_anexados': anexados_coroa
+}
+
+armas = {
+    'Mosquete': 7000,
+    'Baioneta': 3000,
+    'Canhão': 10000,
+    'Espada': 3500,
+    'Pederneira': 5000
+}
+
+def verifica_estado(lista_estados, estado):
+    return estado in lista_estados
+
+def calcular_forca(dic_armas, ouro, soldados, armas):
+    letalidade_armas = 0
+    
+    for arma in armas:
+        letalidade_armas += dic_armas.get(arma)
+    forca = ((ouro * 2) + (soldados * 4) + (letalidade_armas * 4)) / 10
+
+    return forca
+
+def calcular_dano_batalha(estado_atacante, ouro_atacante, soldados_atacante, armas_atacante, estado_defensor, ouro_defensor, soldados_defensor, armas_defensor):
+    ...
